1b5d59a90dcb99cb66524788951456542b767f4d
Matsnord94 <mats_nordstrom@hotmail.com>
2024-01-09 17:15:19 +0100

    v1.5

--- script.js
+++ script.js
@@ -42,64 +42,10 @@ let dealerHit = 0;
 let playerSum = 0;
 let dealerSum = 0;
 let hitResult;
-
+//functiion for translating the card nr into an actual number.
 function translateNumber(number)
 {
-    // for (let i = 0; i < 13; i++) 
-    // {
 
-    //     let a = 0 + i;
-    //     let b = 13 + i;
-    //     let c = 26 + i;
-    //     let d = 39 + i;
-    //     if(number === a  || number === b || number === c || number === d )
-    //     {
-    //         if(i === 0)
-    //         {
-    //             return 11;
-    //         }
-    
-    //         else if(i === 1) 
-    //         {
-    //             return 2;
-    //         }
-    //         else if(i === 2) 
-    //         {
-    //             return 3;
-    //         }
-    //         else if(i === 3) 
-    //         {
-    //             return 4;
-    //         }
-    //         else if(i === 4) 
-    //         {
-    //             return 5;
-    //         }
-    //         else if(i === 5) 
-    //         {
-    //             return 6;
-    //         }
-    //         else if(i === 6) 
-    //         {
-    //             return  7;
-    //         }
-    //         else if(i === 7) 
-    //         {
-    //             return  8;
-    //         }
-    //         else if(i === 8) 
-    //         {
-    //             return 9;
-    //         }
-    //         else if(i === 9 || i === 10 || i === 11 || i === 12) 
-    //         {
-    //            return 10;
-    //         }
-          
-            
-    //     }
-        
-    // }
     const cardValues = [11,2,3,4,5,6,7,8,9,10,10,10,10]
     for (let i = 0; i < 4; i++) {
         let baseIndex = i*13;
@@ -126,6 +72,9 @@ function hit()
 
 
 // Variabler
+    let dealerAceCount = 0;
+    let playerAceCount =0;
+    let gameDraw = false;
     let playerStay = false;
     let playerWin = false;
     let dealerWin = false;
@@ -135,50 +84,82 @@ function hit()
     let antalDealerHits = 0;
 
 async function dealerTurn(){
-while(dealerSum < 17){
+
     
+
+
+while(dealerSum < 17){
     let dealerHit = await hit();
     const translatedNumber =translateNumber(dealerHit);
+    if(translatedNumber === 11){
+        dealerAceCount++;
+    }
     dealerSum += translatedNumber;
     antalDealerHits++;
     let dealerImage = document.getElementById(`dealerContainer${antalDealerHits}`)
     dealerImage.src = imageArray[dealerHit].src;
     console.log(dealerHit);
     document.getElementById('dealerSum').textContent = `Total: ${dealerSum}`;
-    if(dealerSum > 21){
+    if(dealerSum > 21)
+    {
+        if(dealerAceCount > 0)
+        {
+            while(dealerSum > 21 && dealerAceCount > 0)
+            {
+                dealerSum -=10;
+                dealerAceCount--;
+            }
+        }
+        else
+        {
         dealerBust = true;
         playerWin = true;
-    }   
 
+        }
+    }
     else if (dealerSum >= 17 && dealerSum < playerSum){
        playerWin = true;
     }
     
-    else if(dealerSum >= playerSum)
+    else if(dealerSum > playerSum)
     {
         dealerWin =true;
-        
+    }
+
+    else if(dealerSum >= 17 && dealerSum === playerSum)
+    {
+        gameDraw = true;
     }
 
 
-    await new Promise(resolve => setTimeout(resolve, 1500))
-}
+    await new Promise(resolve => setTimeout(resolve, 1000))
 
+}
 checkGameResult();
 
 };
 
 
-async function funcHitButton(){
-    if(!playerBust && !playerWin && !playerStay){
+async function funcHitButton()
+{
+    if(!playerBust && !playerWin && !playerStay && !gameDraw){
     hitResult = await hit();
     antalHits = antalHits +1 ;
     const translatedNumber = translateNumber(hitResult);
+    if(translatedNumber === 11){
+        playerAceCount++;
+        console.log("player ace count = $playerAceCount")
+    }
     playerSum += translatedNumber;
+
+    while (playerSum > 21 && playerAceCount > 0){
+        playerSum -=10;
+        playerAceCount--;
+    }
     let playerImage = document.getElementById(`playerContainer${antalHits}`)
     playerImage.src = imageArray[hitResult].src;
     document.getElementById('playerSum').textContent = `Total: ${playerSum}`;
-    if(playerSum > 21){
+    if(playerSum > 21 && playerAceCount === 0){
         playerBust = true;
         dealerWin = true;
         // resetButton.style.display = 'block';
@@ -189,9 +170,13 @@ async function funcHitButton(){
         playerWin = true;
         checkGameResult();
     }
-   
+    // else if (playerSum > 21 && playerAceCount > 0){
+    //     playerAceCount = playerAceCount - 1;
+    //     playerSum = playerSum -10;
     }
-}
+
+ }
+
 
 function checkGameResult(){
     if(playerWin){
@@ -213,6 +198,12 @@ function checkGameResult(){
             document.getElementById('playerSum').innerHTML = 'BUST';
         }
     }
+    else if(gameDraw){
+        document.getElementById('playerSum').innerHTML = 'PUSH';
+        document.getElementById('dealerSum').innerHTML = 'PUSH';
+        resetButton.style.display = 'block';
+        document.getElementById('reset').textContent = 'Try again!'
+    }
 }
 
 
@@ -230,7 +221,7 @@ hitButton.addEventListener('click', funcHitButton);
 
 let stayButton = document.getElementById('stay');
 stayButton.addEventListener('click', function(){
-    if(!playerBust && !playerStay && !playerWin){
+    if(!playerBust && !playerStay && !playerWin && !gameDraw){
         playerStay = true;
         console.log("Player decided to stay, triggering dealer's turn");
         dealerTurn();
